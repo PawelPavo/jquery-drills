6c00c4419ae266b82ed94995f12d53bb34ec4a5f
PawelPavo <Pawelebllc@gmail.com>
2020-01-13 16:58:52 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//-----------------Simple jQuery Drills-----------------
+
+//Checks the page for code before any script is executed
+// NOTE: this is only neccessary if the scripts are in the <head>.
+$(document).ready(function () {
+    $('#btnSubmit').attr('disabled', true);
+    $('input').keyup(function () {
+        if ($('input').val().length != 0)
+            $('#btnSubmit').attr('disabled', false);
+        else
+            $('#btnSubmit').attr('disabled', true);
+    });
+
+    $('body').append('<div></div>');
+
+    $('body').append('<ul></ul>');
+
+    // $('input').keyup(function () {
+    //     $('#btnSubmit').attr('disabled', false);
+    // });
+
+    $('#btnSubmit').click(function () {
+        event.preventDefault();
+        let text = $('input').val();
+        alert(text); 
+        console.log('Pending files')
+
+        // $('div').append('<h2>' + text + '</h2>');
+        // $('h2').on('mouseover', function () {
+        //     $(this).css({ // How does 'this' work
+        //         'background-color': 'red',
+        //         'border-radious': '10px'
+        //     });
+        // })
+
+        $('ul').append('<li>' + text + '</li>');
+        $('li').on('click', function(){
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            $('li').css({color: `rgb(${r}, ${g}, ${b})`});
+        })
+
+        $('li').on('dblclick', function(){
+            $('li').remove();
+        })
+
+    });
+});
